b6cf96e8afaace2a6294a6ffc1ab9746d4479abe
Diego Padilla <dpadilla@neuromobilemarketing.com>
2023-05-07 14:49:53 +0200

    Allow favorite university

--- src/Controller/UniversityController.php
+++ src/Controller/UniversityController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\UniversityRepository;
+use App\Repository\UserRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use JMS\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -51,6 +53,72 @@ class UniversityController extends AbstractController
         return new Response($response, 200, [
             'Content-Type' => 'application/json'
         ]);
+
+    }
+
+    #[Route('/api/universities/favorite', name: 'set_favorite_university', methods: ['POST'])]
+    public function favoriteUniversity(ManagerRegistry $doctrine, UniversityRepository $universityRepository, UserRepository $userRepository, Request $request)
+    {
+        $decoded = json_decode($request->getContent());
+        $universityId = $decoded->university_id ?? null;
+        $userSub = $decoded->user_sub ?? null;
+
+        if (!isset($universityId) || !isset($userSub)) {
+            return new Response(json_encode(['message' => 'Invalid Request']), 412, ['Content-Type' => 'application/json']);
+        }
+
+        $user = $userRepository->findOneBy(['sub' => $userSub]);
+        $university = $universityRepository->find($universityId);
+
+        if (!$user || !$university) {
+            return new Response(json_encode(['message' => 'Not Found']), 404, ['Content-Type' => 'application/json']);
+        }
+
+        $userUniversity = $user->getUniversities()->filter(function ($university) use ($universityId) {
+            return $university->getId() === $universityId;
+        });
+
+        if ($userUniversity->count() > 0) {
+            $user->getUniversities()->removeElement($university);
+            $university->getUsers()->removeElement($user);
+            $favorite = false;
+        } else {
+            $user->getUniversities()->add($university);
+            $university->getUsers()->add($user);
+            $favorite = true;
+        }
+
+        try {
+            $em = $doctrine->getManager();
+            $em->persist($user);
+            $em->persist($university);
+            $em->flush();
+        } catch (\Exception $e) {
+            return new Response(json_encode(['message' => 'Already favorite']), 500, ['Content-Type' => 'application/json']);
+        }
+
+        return new Response(json_encode(['favorite' => $favorite]), 200, ['Content-Type' => 'application/json']);
+
+    }
+
+    #[Route('/api/universities/favorite/{universityId}/{userSub}', name: 'is_favorite_university', methods: ['GET'])]
+    public function isFavoriteUniversity(UniversityRepository $universityRepository, $universityId, $userSub)
+    {
+        $university = $universityRepository->find($universityId);
+
+        if (!$university) {
+            return new Response(json_encode(['message' => 'Not Found']), 404, ['Content-Type' => 'application/json']);
+        }
+
+        $userUniversity = $university->getUsers()->filter(function ($user) use ($userSub) {
+            return $user->getSub() === $userSub;
+        });
+
+        if ($userUniversity->count() > 0) {
+            return new Response(json_encode(['favorite' => true]), 200, ['Content-Type' => 'application/json']);
+        }
+
+        return new Response(json_encode(['favorite' => false]), 200, ['Content-Type' => 'application/json']);
 
     }
 
