f2f2f5b7cd8647dcc03f91e1ba0e93556c30b53d
Diego Padilla <dpadilla@neuromobilemarketing.com>
2023-05-22 12:59:30 +0200

    Edit composer

--- config/bundles.php
+++ config/bundles.php
@@ -3,6 +3,7 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     DanieleAmbrosino\FirebaseAuthenticationBundle\FirebaseAuthenticationBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
