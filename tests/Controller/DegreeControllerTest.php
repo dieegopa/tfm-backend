78b91140ddeb781eca39ae9eab42afd770377c49
Diego Padilla <dpadilla@neuromobilemarketing.com>
2023-06-16 16:08:15 +0200

    Change localhost

--- tests/Controller/DegreeControllerTest.php
+++ tests/Controller/DegreeControllerTest.php
@@ -23,7 +23,7 @@ class DegreeControllerTest extends KernelTestCase
         static::bootKernel();
 
         self::$client = new Client([
-            'base_uri' => 'https://localhost:8000',
+            'base_uri' => 'https://127.0.0.1:8000',
             'defaults' => [
                 'exceptions' => false
             ]
